e0c7d4eb029252bababb272890d6abf99218367a
chanyong.moon <chanyong.moon@navercorp.com>
2022-11-26 00:50:45 +0900

    chore: exception detail 추가

--- src/main/java/com/yahoconut/problemexample/exception/RandomException.java
+++ src/main/java/com/yahoconut/problemexample/exception/RandomException.java
@@ -1,10 +1,15 @@
 package com.yahoconut.problemexample.exception;
 
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.web.ErrorResponseException;
 
 public class RandomException extends ErrorResponseException {
     public RandomException() {
-        super(HttpStatusCode.valueOf(500));
+        super(
+                HttpStatusCode.valueOf(500),
+                ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(500), "Random failure"),
+                null
+        );
     }
 }
